311a077709a56649b7ff4a2dfc81a1893bf921f1
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-02-24 09:10:32 -0800

    initial commit

---
+++ hello.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_random_wikipedia_article():
+    # URL for random Wikipedia article
+    url = "https://en.wikipedia.org/wiki/Special:Random"
+
+    # Send request and get response
+    response = requests.get(url)
+
+    # Parse HTML content
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Get article title
+    title = soup.find(id="firstHeading").text
+
+    # Get main content (excluding references, navigation, etc.)
+    content = soup.find(id="mw-content-text")
+
+    # Remove unwanted elements
+    for unwanted in content.find_all(['table', 'sup', 'div.thumb', 'span.mw-editsection']):
+        unwanted.decompose()
+
+    # Extract text
+    text = content.get_text().strip()
+
+    return {
+        'title': title,
+        'text': text
+    }
+
+# Example usage
+if __name__ == "__main__":
+    article = get_random_wikipedia_article()
+    print(f"Title: {article['title']}\n")
+    print(article['text'][:500] + "...")  # Print first 500 characters
