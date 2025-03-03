34d370c7dd2d5225463502fb7ad4b3ee6ab0e7be
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-03-03 15:33:03 -0800

    add nix flake direnv config

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+    with nixpkgs.legacyPackages.${system};
+    let
+      scripts = {};
+    in {
+      devShell = stdenvNoCC.mkDerivation {
+        name = "shell";
+        buildInputs = lib.attrsets.mapAttrsToList writeShellScriptBin scripts ++ [
+
+        ];
+      };
+    });
+}
